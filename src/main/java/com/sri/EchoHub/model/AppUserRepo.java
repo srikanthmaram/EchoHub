2801e5113f5e0781d1f45a81e15a61ccedacd577
srikanthmaram <maramsrikanth08@gmail.com>
2025-01-19 12:28:13 +0530

    new docker

--- src/main/java/com/sri/EchoHub/model/AppUserRepo.java
+++ src/main/java/com/sri/EchoHub/model/AppUserRepo.java
@@ -1,11 +1,11 @@
-package com.sri.EchoHub.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-
-@Repository
-public interface AppUserRepo extends JpaRepository<AppUser,Integer> {
-
-
-}
+//package com.sri.EchoHub.model;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//
+//@Repository
+//public interface AppUserRepo extends JpaRepository<AppUser,Integer> {
+//
+//
+//}
